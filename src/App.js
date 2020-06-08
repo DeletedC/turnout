81b7d7a4774c0f255d3e8e5db58fcc47b87e596f
Curtis <CurtisWoodsArt@gmail.com>
2020-06-08 18:21:27 -0400

    Added starter code for getting events and blank form data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import "./style.css";
 
 const App = (props) => {
 
+  // State to hold all events
+  const [events, setEvents] = React.useState(null);
+
+  // State to hold the event the user wants to edit
+  const [eventToEdit, setEventToEdit] = React.useState({
+    // Add blank event form data here
+  });
+
+  // Holds blank form data
+  const blank = {
+    // Add blank event form data here
+  }
+
+  // Hook to get events when the component loads
+  React.useEffect(() => {
+    //getEvents();
+  }, []);
+
+  // Get the events from the API
+  const getEvents = async () => {
+    //const response = await fetch(<----FILL IN API CALL HERE ---->);
+    //const result = await response.json();
+    //setEvents(result);
+  };
+
+  
 
   return (
     <>
