791ced01b6ea4bf84b87b4acaa70f092dbee8867
caitlin farrell <farrellcaite@gmail.com>
2020-06-13 23:36:44 -0400

    event page styled, figured out dropdown for hover

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "./style.scss";
 import Form from "./form.js"
-import logo from "./imgs/logo-name.png"
 import img from "./imgs/logo.png"
 import MainNav from "./MainNav/index"
 
@@ -71,52 +70,39 @@ const App = (props) => {
 
   return (
     <>
-    <MainNav/>
+    
       <header>
-        <h1>Organize from anywhere.<br/> Find local gatherings you believe in.</h1>
+        <h1>Organize from anywhere.</h1><br/>
+        <h2>Find local gatherings you believe in.</h2>
       </header>
+      <div>
+          <img src={img} alt="Img" className="small-logo"/>
+        </div>
       <ul className="event-list">
         {events? events.map((item) => {
 
             return (
-              <li key={item._id} className="listed-event"><Link to="./show">{item.title}</Link>
-                <br/>
-                {/* <button onClick={() => {handleSelect(item)}}>
-                  Edit
-                </button> */}
-                <button>
-                  <Link to={{pathname: './edit', state: {item}}}>
-                    Edit
-                  </Link>
-                </button>
-                <button onClick={() => {handleDelete(item._id)}}>
-                  Delete
-                </button>
-                  <div className="event-snippet">
-                    <p>Category: {item.category}</p>
-                    <p>Date: {item.date}</p>
-                    <p>Location: {item.location}</p>
-                  </div>
-              </li>
+              <div className="event-items">
+              <div className="dropdown">
+                <li key={item._id} className="listed-event"><Link to="./show">{item.title}</Link></li>
+              <div class="dropdown-content">
+                <p className="event-details">{item.category}</p>
+                <p className="event-details">{item.date}</p>
+                <p className="event-details">{item.location}</p>
+                </div>
+              </div>
+              </div>
             );
           })
           : <li>Loading...</li>
         }
       </ul>
-      <div class="event-handlers">
-        
-          {/* <Form initial={blank} handleSubmit={handleCreate}/> */}
-        
-        <div>
-          <img src={img} alt="Img"/>
-        </div>
-        <div id="edit-event">
-          <h2 className="edit">Edit Event</h2>
-          <Form initial={eventToEdit} handleSubmit={handleEdit}/>
-        </div>
-      </div>
+      
     </>
   );
 }
 
 export default App;
+
+
+
