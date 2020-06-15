c5f14099dc1f0fd8fe75ff4d4f38af2c3295304e
Curtis <CurtisWoodsArt@gmail.com>
2020-06-14 23:53:07 -0400

    Edit page, removed unused imports, test console logs, and cleaned up code for readability

--- src/Edit/index.js
+++ src/Edit/index.js
@@ -1,59 +1,50 @@
-import React, {Component} from "react";
-import "../style.css";
+import React from "react";
+import "../style.scss";
 import Form from "../form.js"
-import { Link, useHistory, Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 export default (props)=>{
 
-    const history = useHistory()
+  const history = useHistory()
 
+  const [eventToEdit, setEvents] = React.useState({
+    title: "",
+    category: "",
+    date: "",
+    location: "",
+    images: [],
+    attendees: []
+  });
 
-     const [eventToEdit, setEvents] = React.useState({
-        title: "",
-        category: "",
-        date: "",
-        location: "",
-        images: [],
-        attendees: []
-      });
+  const getEvents = async () => {
+    const response = await fetch('https://turnout-nova-api.herokuapp.com/events');
+    const result = await response.json();
 
-      const getEvents = async () => {
-        const response = await fetch('https://turnout-nova-api.herokuapp.com/events');
-        const result = await response.json();
-        
-        // Test console.log
-        console.log(result);
-        setEvents(result);
-      };
+    setEvents(result);
+  };
 
-     const handleEdit = async (item) => {
-        const response = await fetch(`https://turnout-nova-api.herokuapp.com/events/${item._id}`, {
-          method: "PUT",
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(item)
-        });
-        getEvents(); // Update list of events
-        history.push("/")
-      };
+  const handleEdit = async (item) => {
+    const response = await fetch(`https://turnout-nova-api.herokuapp.com/events/${item._id}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    });
+    getEvents(); // Update list of events
+    history.push("/")
+  };
 
-    //   const handleSelect = async (item) => {
-    //     setEventToEdit(item);
-    //   };
-
-      return (
-        <body className="edit-container">
-          <div className="edit-page">
-          {console.log(props)}
-          <h2 className="edit-title">Edit Your Event</h2>
-          <p className="edit-descrip">Make any necessary changes to your event here.</p>
-          <div className="edit-event">
+  return (
+    <body className="edit-container">
+      <div className="edit-page">
+        <h2 className="edit-title">Edit Your Event</h2>
+        <p className="edit-descrip">Make any necessary changes to your event here.</p>
+        <div className="edit-event">
           <Form initial={props.location.state.item} handleSubmit={handleEdit}/>
-          </div>
-          </div>
-        </body>
-        
-      )
+        </div>
+      </div>
+    </body>
+  )
 }
