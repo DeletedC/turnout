44fde25b21ed7070b656ee9deebfee691f286ad1
Curtis <CurtisWoodsArt@gmail.com>
2020-06-14 23:40:43 -0400

    New page, removed unused imports, cleaned up code for readability

--- src/New/index.js
+++ src/New/index.js
@@ -1,37 +1,33 @@
-import React, {Component} from "react";
+import React from "react";
 import "../style.css";
 import Form from "../form.js"
-import logo from "../imgs/logo-name.png"
-import { Link, useHistory, Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 export default (New)=>{
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const [events, setEvents] = React.useState(null);
-    const blank = {
-        title: "",
-        category: "",
-        date: "",
-        location: "",
-        images: [],
-        attendees: []
-        
-      };
+  const [events, setEvents] = React.useState(null);
+  const blank = {
+      title: "",
+      category: "",
+      date: "",
+      location: "",
+      images: [],
+      attendees: []
+    };
 
-      const getEvents = async () => {
-        const response = await fetch('https://turnout-nova-api.herokuapp.com/events');
-        const result = await response.json();
-        
-        // Test console.log
-        console.log(result);
-        setEvents(result);
-      };
+  const getEvents = async () => {
+    const response = await fetch('https://turnout-nova-api.herokuapp.com/events');
+    const result = await response.json();
 
-     /// / Create a new event
+    setEvents(result);
+  };
+
+  // Create a new event
   const handleCreate = async (data) => {
-    const response = await fetch('https://turnout-nova-api.herokuapp.com/events', {
+    await fetch('https://turnout-nova-api.herokuapp.com/events', {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
@@ -40,20 +36,20 @@ export default (New)=>{
     });
     
     getEvents(); // Update the list of events
-    console.log(response);
     history.push("/");
     
   };
-      return (
-          <body className="create-container">
-              <div class="create-page">
-     <h2 className="create-title">Planning an event?</h2>
-     <h3 className="second-title">We're here to help.</h3>
-     <p className="create-descrip">Holding a vigil or a march? Is the march on foot or bike-centric?<br/> Use this form to create your gathering.</p>
-      <div className="create-event">
-      <Form initial={blank} handleSubmit={handleCreate}/>
-      </div>
+
+  return (
+    <body className="create-container">
+      <div class="create-page">
+        <h2 className="create-title">Planning an event?</h2>
+        <h3 className="second-title">We're here to help.</h3>
+        <p className="create-descrip">Holding a vigil or a march? Is the march on foot or bike-centric?<br/> Use this form to create your gathering.</p>
+        <div className="create-event">
+          <Form initial={blank} handleSubmit={handleCreate}/>
+        </div>
       </div>
     </body>
-      )
+  )
 }
